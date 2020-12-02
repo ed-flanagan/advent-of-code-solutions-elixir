92776509e441e4fb780def080bc4d003ac4bf8b6
Ed Flanagan <ed@flanagan.xyz>
2020-12-01 23:48:39 -0800

    Mix fixes

--- lib/advent/y2015/01_not_quite_lisp.ex
+++ lib/advent/y2015/01_not_quite_lisp.ex
@@ -10,7 +10,7 @@ defmodule Advent.Y2015.D01 do
   @spec part_two(input :: charlist()) :: integer()
   def part_two(input) do
     input
-    |> Stream.with_index
+    |> Stream.with_index()
     |> Enum.reduce_while(0, fn
       {?), i}, 0 -> {:halt, i + 1}
       {?), _}, acc -> {:cont, acc - 1}
