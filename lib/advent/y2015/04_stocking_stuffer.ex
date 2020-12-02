ecbbe5655e98f85aa0ecce474c01dddb7c5d2a0b
Ed Flanagan <ed@flanagan.xyz>
2020-12-02 02:41:40 -0800

    Solution 2015 Day 4

---
+++ lib/advent/y2015/04_stocking_stuffer.ex
@@ -0,0 +1,29 @@
+defmodule Advent.Y2015.D04 do
+  def part_one(key, limit) do
+    brute_search_md5_pad(0..limit, key, "00000")
+  end
+
+  def part_two(key, limit) do
+    brute_search_md5_pad(0..limit, key, "000000")
+  end
+
+  # NOTE: I originally implimented using multiple function heads, e.g.
+  # `brute_search("00000" <> _rest, ...)`
+  # However, it wasn't portable with the different searches
+  # String.starts_with? does a binary prefix check, so should still be fast
+  # I also wanted to force the use of a hard ceiling to help limit run-away
+  # searches
+  defp brute_search_md5_pad(start..stop, key, hash_pad) do
+    start..stop
+    |> Stream.map(&Integer.to_string/1)
+    |> Enum.find(fn i ->
+      :crypto.hash(:md5, key <> i)
+      |> Base.encode16()
+      |> String.starts_with?(hash_pad)
+    end)
+    |> case do
+      nil -> :error
+      i -> String.to_integer(i)
+    end
+  end
+end
---
+++ test/y2015/04_stocking_stuffer_test.exs
@@ -0,0 +1,36 @@
+defmodule Advent.Y2015.D04Test do
+  use ExUnit.Case
+
+  alias Advent.Y2015.D04
+  import D04, only: [part_one: 2, part_two: 2]
+
+  describe "part_one/2" do
+    test "solves example input" do
+      [
+        {"abcdef", 609_043},
+        {"pqrstuv", 1_048_970}
+      ]
+      |> Enum.each(fn {input, expected} ->
+        assert expected == part_one(input, 1_200_000)
+      end)
+    end
+
+    test "solves puzzle input" do
+      assert 117_946 == part_one("ckczppom", 150_000)
+    end
+
+    test "limit works" do
+      assert :error == part_one("deadbeef", 5)
+    end
+  end
+
+  describe "part_two/2" do
+    test "solves puzzle input" do
+      assert 3_938_038 == part_two("ckczppom", 4_000_000)
+    end
+
+    test "limit works" do
+      assert :error == part_two("deadbeef", 5)
+    end
+  end
+end
