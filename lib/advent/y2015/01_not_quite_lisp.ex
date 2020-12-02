89fca51eebae002e9e0f5c9122666d7515db3042
Ed Flanagan <ed@flanagan.xyz>
2020-12-01 21:24:49 -0800

    Solution 2015 day 1

---
+++ lib/advent/y2015/01_not_quite_lisp.ex
@@ -0,0 +1,20 @@
+defmodule Advent.Y2015.D01 do
+  @spec part_one(input :: charlist()) :: integer()
+  def part_one(input) do
+    Enum.reduce(input, 0, fn
+      ?(, acc -> acc + 1
+      ?), acc -> acc - 1
+    end)
+  end
+
+  @spec part_two(input :: charlist()) :: integer()
+  def part_two(input) do
+    input
+    |> Stream.with_index
+    |> Enum.reduce_while(0, fn
+      {?), i}, 0 -> {:halt, i + 1}
+      {?), _}, acc -> {:cont, acc - 1}
+      {?(, _}, acc -> {:cont, acc + 1}
+    end)
+  end
+end
---
+++ test/y2015/01_not_quite_lisp.ex
@@ -0,0 +1,55 @@
+defmodule Advent.Y2015.D01Test do
+  use ExUnit.Case
+
+  alias Advent.Y2015.D01
+
+  describe "part_one/1" do
+    test "solves example input" do
+      examples = [
+        {'(())', 0},
+        {'()()', 0},
+        {'(((', 3},
+        {'(()(()(', 3},
+        {'))(((((', 3},
+        {'())', -1},
+        {'))(', -1},
+        {')))', -3},
+        {')())())', -3}
+      ]
+
+      Enum.each(examples, fn {input, expected} ->
+        assert expected == D01.part_one(input)
+      end)
+    end
+
+    test "solves puzzle input" do
+      assert 138 == D01.part_one(puzzle_input())
+    end
+  end
+
+  describe "part_two/1" do
+    test "solves example input" do
+      examples = [
+        {')', 1},
+        {'()())', 5}
+      ]
+
+      Enum.each(examples, fn {input, expected} ->
+        assert expected == D01.part_two(input)
+      end)
+    end
+
+    test "solves puzzle input" do
+      assert 1771 == D01.part_two(puzzle_input())
+    end
+  end
+
+  defp puzzle_input() do
+    Path.join([__DIR__, "support", "01_input.txt"])
+    |> File.stream!()
+    |> Stream.map(&String.trim/1)
+    |> Enum.take(1)
+    |> List.first()
+    |> to_charlist
+  end
+end
