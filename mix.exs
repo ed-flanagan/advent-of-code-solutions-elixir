8fd97d4e66c3e1aef3aaccb3e1c1b85b12afc61c
Ed Flanagan <ed@flanagan.xyz>
2021-11-24 23:19:19 -0800

    Update mix dependencies
    * Add credo, dialyxir, stream_data
    * Add stream_data to formatter
    * Remove unused, default extra applications
    * Check-in mix.lock file

--- mix.exs
+++ mix.exs
@@ -11,18 +11,13 @@ defmodule Advent.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:crypto, :logger]
-    ]
-  end
+  def application, do: []
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:stream_data, "~> 0.5", only: [:dev, :test]}
     ]
   end
 end
