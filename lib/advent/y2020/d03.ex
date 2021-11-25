2380de6382c3de3fa913089ee7a2b7cd4fcf5f17
Ed Flanagan <ed@flanagan.xyz>
2021-11-25 00:37:23 -0800

    Fix 2020 Day 3 specs

--- lib/advent/y2020/d03.ex
+++ lib/advent/y2020/d03.ex
@@ -3,19 +3,19 @@ defmodule Advent.Y2020.D03 do
   https://adventofcode.com/2020/day/3
   """
 
-  @spec part_one(rows :: Enumerable.t(String.t())) :: integer
+  @spec part_one(Enumerable.t()) :: integer()
   def part_one(rows) do
     count_trees(rows, 3, 1)
   end
 
-  @spec part_two(rows :: Enumerable.t(String.t()), strategies :: Enumerable.t({integer, integer})) ::
-          integer
+  @spec part_two(Enumerable.t(), [{integer, integer}]) :: integer()
   def part_two(rows, strategies) do
     Enum.reduce(strategies, 1, fn {right, down}, product ->
       product * count_trees(rows, right, down)
     end)
   end
 
+  @spec count_trees(Enumerable.t(), integer(), integer()) :: integer()
   defp count_trees(rows, right, down) do
     # NOTE: this assumes each row is uniform in size. If that's not the case,
     # this would need to be computed for each row
