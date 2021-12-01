3123c3ba241581b54f72b3f00063ef5f3af4ae1d
Ed Flanagan <ed@flanagan.xyz>
2021-12-01 14:47:13 -0800

    Solution 2019 Day 01

---
+++ lib/advent/y2019/d01.ex
@@ -0,0 +1,27 @@
+defmodule Advent.Y2019.D01 do
+  @moduledoc """
+  https://adventofcode.com/2019/day/1
+  """
+
+  @spec part_one(Enumerable.t()) :: integer()
+  def part_one(input) do
+    input
+    |> Stream.map(&module_fuel/1)
+    |> Enum.sum()
+  end
+
+  @spec part_one(Enumerable.t()) :: integer()
+  def part_two(input) do
+    input
+    |> Stream.map(fn mass ->
+      Stream.unfold(module_fuel(mass), fn
+        m when m > 0 -> {m, module_fuel(m)}
+        _ -> nil
+      end)
+      |> Enum.sum()
+    end)
+    |> Enum.sum()
+  end
+
+  defp module_fuel(mass), do: div(mass, 3) - 2
+end
