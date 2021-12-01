92ed753a918504ceae304e9d3c7393b5c6740b42
Ed Flanagan <ed@flanagan.xyz>
2021-12-01 01:49:17 -0800

    Solution 2021 Day 01

---
+++ lib/advent/y2021/d01.ex
@@ -0,0 +1,27 @@
+defmodule Advent.Y2021.D01 do
+  @moduledoc """
+  https://adventofcode.com/2021/day/1
+  """
+
+  @doc """
+  Count the number of times a depth measurement increases from the previous
+  measurement 
+  """
+  @spec part_one(Enumerable.t()) :: non_neg_integer()
+  def part_one(input) do
+    input
+    |> Stream.chunk_every(2, 1, :discard)
+    |> Enum.count(fn [a, b] -> b > a end)
+  end
+
+  @doc """
+  Consider sums of a three-measurement sliding window. How many sums are larger
+  than the previous sum?
+  """
+  @spec part_two(Enumerable.t()) :: non_neg_integer()
+  def part_two(input) do
+    input
+    |> Stream.chunk_every(4, 1, :discard)
+    |> Enum.count(fn [a, _, _, b] -> b > a end)
+  end
+end
---
+++ test/y2021/d01_test.exs
@@ -0,0 +1,46 @@
+defmodule Advent.Y2021.D01Test do
+  use ExUnit.Case
+
+  alias Advent.Y2021.D01
+
+  @example_input [
+    199,
+    200,
+    208,
+    210,
+    200,
+    207,
+    240,
+    269,
+    260,
+    263
+  ]
+
+  describe "part_one/1" do
+    test "solves example input" do
+      assert D01.part_one(@example_input) == 7
+    end
+
+    test "solves puzzle input" do
+      assert D01.part_one(puzzle_input!()) == 1226
+    end
+  end
+
+  describe "part_one/2" do
+    test "solves example input" do
+      assert D01.part_two(@example_input) == 5
+    end
+
+    test "solves puzzle input" do
+      assert D01.part_two(puzzle_input!()) == 1252
+    end
+  end
+
+  @spec puzzle_input!() :: Enumerable.t()
+  def puzzle_input!() do
+    Path.join([__DIR__, "support", "d01_input.txt"])
+    |> File.stream!()
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.to_integer/1)
+  end
+end
