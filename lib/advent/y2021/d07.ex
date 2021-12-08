70f9bf1b4df3dac60cbbc3829df9dbea2680230c
Ed Flanagan <ed@flanagan.xyz>
2021-12-07 21:59:26 -0800

    Solution 2021 Day 07

---
+++ lib/advent/y2021/d07.ex
@@ -0,0 +1,51 @@
+defmodule Advent.Y2021.D07 do
+  @moduledoc """
+  https://adventofcode.com/2021/day/7
+  """
+
+  @doc """
+  Determine the horizontal position that the crabs can align to using the least
+  fuel possible. How much fuel must they spend to align to that position?
+  """
+  @spec part_one([integer()]) :: integer()
+  def part_one(input) do
+    median = median(input)
+
+    Enum.reduce(input, 0, fn x, acc ->
+      acc + abs(x - median)
+    end)
+  end
+
+  @doc """
+  Determine the horizontal position that the crabs can align to using the least
+  fuel possible so they can make you an escape route! How much fuel must they
+  spend to align to that position?
+  """
+  @spec part_two([integer()]) :: integer()
+  def part_two(input) do
+    Enum.reduce_while(Enum.min(input)..Enum.max(input), :infinity, fn m, lowest ->
+      result =
+        Enum.reduce(input, 0, fn x, acc ->
+          acc + sum_consecutive_ints(abs(x - m))
+        end)
+
+      if result > lowest do
+        {:halt, lowest}
+      else
+        {:cont, result}
+      end
+    end)
+  end
+
+  @spec median([integer()]) :: integer()
+  defp median(vals) do
+    vals
+    |> Enum.sort()
+    |> Enum.at(div(length(vals), 2))
+  end
+
+  @spec sum_consecutive_ints(pos_integer()) :: pos_integer()
+  defp sum_consecutive_ints(n) do
+    trunc(n * (n + 1) / 2)
+  end
+end
---
+++ test/advent/y2021/d07_test.exs
@@ -0,0 +1,37 @@
+defmodule Advent.Y2021.D07Test do
+  use ExUnit.Case
+
+  import Advent.Y2021.D07, only: [part_one: 1, part_two: 1]
+
+  @example_input [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+
+  describe "part_one/1" do
+    test "solves example input" do
+      assert part_one(@example_input) == 37
+    end
+
+    test "solves puzzle input" do
+      assert part_one(puzzle_input!()) == 347_509
+    end
+  end
+
+  describe "part_one/2" do
+    test "solves example input" do
+      assert part_two(@example_input) == 168
+    end
+
+    test "solves puzzle input" do
+      assert part_two(puzzle_input!()) == 98_257_206
+    end
+  end
+
+  @spec puzzle_input!() :: Enumerable.t()
+  defp puzzle_input!() do
+    Path.join([__DIR__, "support", "d07_input.txt"])
+    |> File.stream!()
+    |> Stream.map(&String.trim/1)
+    |> Enum.at(0)
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+end
