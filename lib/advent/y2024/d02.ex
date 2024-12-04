4d359381d07234ec94846dae794d49291620ded8
Ed Flanagan <ed@flanagan.xyz>
2024-12-03 17:18:49 -0800

    [2024-02] Init

---
+++ lib/advent/y2024/d02.ex
@@ -0,0 +1,19 @@
+defmodule Advent.Y2024.D02 do
+  @moduledoc """
+  https://adventofcode.com/2024/day/2
+  """
+
+  @doc """
+  """
+  @spec part_one(Enumerable.t()) :: any()
+  def part_one(_input) do
+    0
+  end
+
+  @doc """
+  """
+  @spec part_two(Enumerable.t()) :: any()
+  def part_two(_input) do
+    0
+  end
+end
---
+++ test/advent/y2024/d02_test.exs
@@ -0,0 +1,20 @@
+defmodule Advent.Y2024.D02Test do
+  use TestHelper
+
+  @example_input [
+    "7 6 4 2 1",
+    "1 2 7 8 9",
+    "9 7 6 2 1",
+    "1 3 2 4 5",
+    "8 6 4 4 1",
+    "1 3 6 7 9"
+  ]
+
+  aoc_test(
+    example_input: @example_input,
+    p1_example_solution: 2
+    # p1_solution: 0
+    # p2_example_solution: 0
+    # p2_solution: 0
+  )
+end
