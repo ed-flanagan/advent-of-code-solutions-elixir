0727573cbb1fd2123fbf75c0f733fa847637ae8d
Ed Flanagan <ed@flanagan.xyz>
2024-12-03 23:19:31 -0800

    [2024-02] Part 2

--- lib/advent/y2024/d02.ex
+++ lib/advent/y2024/d02.ex
@@ -9,34 +9,45 @@ defmodule Advent.Y2024.D02 do
   def part_one(input) do
     input
     |> parse_input()
-    |> Enum.map(&is_safe/1)
-    |> Enum.sum()
+    |> Enum.map(&safe?/1)
+    |> Enum.count(&(&1 == :safe))
   end
 
   @doc """
   """
   @spec part_two(Enumerable.t()) :: any()
-  def part_two(_input) do
-    0
+  def part_two(input) do
+    input
+    |> parse_input()
+    |> Enum.map(&tolerant?/1)
+    |> Enum.count(&(&1 == :safe))
   end
 
-  defp is_safe(report = [x, y | _]) do
+  defp safe?(report = [x, y | _]) do
     cond do
-      x < y -> is_safe(report, &Kernel.</2)
-      x > y -> is_safe(report, &Kernel.>/2)
-      true -> 0
+      x < y -> safe?(report, &Kernel.</2)
+      x > y -> safe?(report, &Kernel.>/2)
+      true -> :unsafe
     end
   end
 
-  defp is_safe([], _op), do: 1
-  defp is_safe([_], _op), do: 1
+  defp safe?([], _op), do: :safe
+  defp safe?([_], _op), do: :safe
 
-  defp is_safe([x, y | rest], op) do
+  defp safe?([x, y | rest], op) do
     if op.(x, y) && abs(y - x) in 1..3 do
-      is_safe([y | rest], op)
+      safe?([y | rest], op)
     else
-      0
+      :unsafe
     end
+  end
+
+  defp tolerant?(report) do
+    Stream.unfold(length(report), fn
+      -1 -> nil
+      idx -> {safe?(List.delete_at(report, idx)), idx - 1}
+    end)
+    |> Enum.find(:unsafe, &(&1 == :safe))
   end
 
   @spec parse_input(Enumerable.t()) :: Enumerable.t()
