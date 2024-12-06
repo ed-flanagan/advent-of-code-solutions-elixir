1d38543c9915ea5c5074726d15a17f8807e92dea
Ed Flanagan <ed@flanagan.xyz>
2024-12-06 02:09:36 -0800

    [2024-03] part 2

--- lib/advent/y2024/d03.ex
+++ lib/advent/y2024/d03.ex
@@ -8,21 +8,40 @@ defmodule Advent.Y2024.D03 do
   @spec part_one(Enumerable.t()) :: non_neg_integer()
   def part_one(input) do
     input
-    |> Enum.flat_map(fn line ->
-      Regex.scan(~r/mul\((\d+),(\d+)\)/, line, capture: :all_but_first)
-    end)
-    |> Enum.map(fn pair ->
-      pair
-      |> Enum.map(&String.to_integer/1)
-      |> Enum.reduce(&Kernel.*/2)
-    end)
+    |> parse_input()
+    |> then(&Regex.scan(~r/mul\((\d+),(\d+)\)/, &1, capture: :all_but_first))
+    |> Enum.map(&mult_pair/1)
     |> Enum.sum()
   end
 
   @doc """
   """
-  @spec part_two(Enumerable.t()) :: any()
-  def part_two(_input) do
-    0
+  @spec part_two(Enumerable.t()) :: non_neg_integer()
+  def part_two(input) do
+    input
+    |> parse_input()
+    |> then(&Regex.scan(~r/(mul)\((\d+),(\d+)\)|don't\(\)|do\(\)/, &1))
+    |> Enum.map(fn
+      [_, "mul" | nums] -> mult_pair(nums)
+      ["don't()"] -> :disable
+      ["do()"] -> :enable
+    end)
+    |> Enum.reduce({:enable, 0}, fn
+      :enable, {_state, sum} -> {:enable, sum}
+      :disable, {_state, sum} -> {:disable, sum}
+      n, {:enable, sum} -> {:enable, sum + n}
+      _n, acc -> acc
+    end)
+    |> elem(1)
+  end
+
+  defp mult_pair(pair = [_x, _y]) do
+    pair
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce(&Kernel.*/2)
+  end
+
+  defp parse_input(input) do
+    Enum.join(input)
   end
 end
--- test/advent/y2024/d03_test.exs
+++ test/advent/y2024/d03_test.exs
@@ -1,15 +1,17 @@
 defmodule Advent.Y2024.D03Test do
   use TestHelper
 
+  # Alt. given:
+  # "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
   @example_input [
-    "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
   ]
 
   aoc_test(
     example_input: @example_input,
     p1_example_solution: 161,
-    p1_solution: 167_090_022
-    # p2_example_solution: 0
-    # p2_solution: 0
+    p1_solution: 167_090_022,
+    p2_example_solution: 48,
+    p2_solution: 89_823_704
   )
 end
